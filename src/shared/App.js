4dff1feee28e59368c83dd125d89e3acb854ad34
hoyeon96 <hoyen96@naver.com>
2021-06-26 02:22:21 +0900

    서브라우트 설정방법

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route ,Switch } from 'react-router-dom';
+import About from '../pages/About';
+import Home from '../pages/Home';
+import Posts from '../pages/Posts';
+
+import Header from '../components/Header';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Hello React-Router
-            </div>
+            <Router>
+                <div>
+                    <Header  />
+                    <Route exact path="/" component={Home} />
+                    {/* exact를 하지 않으면 /about에도 /가 있기 때문에, 매칭되어 보여짐 */}
+                    <Switch>
+                        <Route path="/about/:name" component={About} />
+                        <Route path="/about " component={About} />
+                        {/* /about을 위에 작성했더니 name 입력해도 안나타남 */}
+                        <Route path="/posts" component={Posts} />
+                    </Switch>
+                </div>
+            </Router>
+                
+            
         );
     }
 }
