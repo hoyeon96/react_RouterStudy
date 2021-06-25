079c6631b4cccd2de878c19ac3d5ba6d50e69d8f
hoyeon96 <hoyen96@naver.com>
2021-06-26 03:25:54 +0900

    switch를 이용해 404페이지 만들기

--- src/shared/App.js
+++ src/shared/App.js
@@ -6,6 +6,7 @@ import Posts from '../pages/Posts';
 import MyPage from '../pages/MyPage';
 import Login from '../pages/Login';
 import Search from '../pages/Search';
+import NoMatch from '../pages/NoMatch';
 
 import Header from '../components/Header';
 
@@ -15,17 +16,23 @@ class App extends Component {
             <Router>
                 <div>
                     <Header  />
-                    <Route exact path="/" component={Home} />
-                    {/* exact를 하지 않으면 /about에도 /가 있기 때문에, 매칭되어 보여짐 */}
-                    <Switch>
-                        <Route path="/about/:name" component={About} />
-                        <Route path="/about " component={About} />
-                        {/* /about을 위에 작성했더니 name 입력해도 안나타남 */}
-                    </Switch>
-                    <Route path="/posts" component={Posts} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/me" component={MyPage} />
-                    <Route path="/search" component={Search} />
+                    <div>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            {/* exact를 하지 않으면 /about에도 /가 있기 때문에, 매칭되어 보여짐 */}
+                            {/* <Switch> */}
+                                <Route path="/about/:name" component={About} />
+                                {/* <Route path="/about " component={About} /> */}
+                                {/* /about을 위에 작성했더니 name 입력해도 안나타남 */}
+                            {/* </Switch> */}
+                            <Route path="/posts" component={Posts} />
+                            <Route path="/login" component={Login} />
+                            <Route path="/me" component={MyPage} />
+                            <Route path="/search" component={Search} />
+                            <Route component={NoMatch} />
+                        </Switch>
+                    </div>
+                    
                 </div>
             </Router>
                 
